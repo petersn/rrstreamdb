aa0db6425c8c63c476e55061210a0ecb67f8245f
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2022-01-10 16:31:40 -0800

    More changes.

---
+++ widgetshare/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+
+  // file resolutions
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+
+  // loaders
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
